ae59ec0c5d89551d23201595dc8d9e5d65cd2daf
Alice Hsing <alicehsing@gmail.com>
2022-01-10 12:31:10 -0800

    fetch and display families from supabase and render them to the page

--- fetch-utils.js
+++ fetch-utils.js
@@ -9,12 +9,20 @@ export async function getUser() {
 
 export async function getFamilies() {
     // fetch all families and their bunnies
+    const response = await client
+        .from('loving_families')
+        .select('*, fuzzy_bunnies (*)');
 
     return checkError(response);    
 }
 
 export async function deleteBunny(id) {
     // delete a single bunny using the id argument
+    const response = await client
+        .from('fuzzy_bunnies')
+        .delete()
+        .match({ id: id })
+        .single();
 
     return checkError(response);    
 }
